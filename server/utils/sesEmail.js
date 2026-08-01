0a1e6cd8cfa3aa3ce180d9f25998b23a896c19fc
Aryan Patel <aaryanpatel9784@gmail.com>
2026-08-02 04:08:10 +0530

    feat: delete unnessesory files from project

--- server/utils/sesEmail.js
+++
@@ -1,4 +0,0 @@
-/**
- * @deprecated Import from ./awsSes.js instead. Kept for backward compatibility.
- */
-export { sendBookingEmail, getVerifiedSenderAddress, logSesEnvironmentStatus } from './awsSes.js';
